a0a998399e3dc412f747e35cc6a17527143454a3
Veager <viliussaukstas@gmail.com>
2021-08-13 20:59:12 +0300

    added bare-bones calculations

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
     input:
     input ends with operator
     operator1
+    operator as first input
+    bracket between number and factorial function
 */
 
 //functions for mathematical operations
-function add(op1, op2){
-    return op1 + op2;
+function add(a, b){
+    return parseInt(a) + parseInt(b);
 }
 function subtract(op1, op2){
     return op1 - op2;
@@ -33,16 +35,48 @@ function addInput(e){
             console.log("Two operators in a row")
             return 0;
         }
-        display.innerText += e.target.innerText;
 
+        display.innerText += e.target.innerText;
         displayIndex++;
         displayArray[displayIndex] = e.target.innerText;
+
+        let operatorPriority;
+        switch(e.target.innerText){
+            case "+": operatorPriority=1; break;
+            case "-": operatorPriority=1; break;
+            case "*": operatorPriority=2; break;
+            case "/": operatorPriority=2; break;
+        }
+        let operatorObject = {
+            symbol: e.target.innerText,
+            index: displayIndex,
+            priority: operatorPriority,
+        }
+        orderOfOperations.push(operatorObject);
+
         displayIndex++;
         displayArray[displayIndex] = ""; //prepares array element for next input
-
-        orderOfOperations.push(e.target.innerText);
         prevWasOperator = true;
     }
+    else if(e.target.innerText === "="){
+        calculate();
+    }
     console.log(displayArray);
-    console.log(orderOfOperations);
+    console.table(orderOfOperations);
+}
+
+function calculate(){
+    orderOfOperations.sort((a, b) => b.priority-a.priority);
+    console.log("were in");
+    while(orderOfOperations.length>0){
+        let result;
+        switch(orderOfOperations[0].symbol){
+            case "+": result = add(displayArray[orderOfOperations[0].index-1], displayArray[orderOfOperations[0].index+1]); break;
+            case "-": result = subtract(displayArray[orderOfOperations[0].index-1], displayArray[orderOfOperations[0].index+1]); break;
+            default: console.log("none of them?");
+        }
+        display.innerText = result;
+        orderOfOperations.shift();
+    }
+    console.log("calculated");
 }
