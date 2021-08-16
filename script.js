ba86539e7038c9ebd382eca4cdb620917291d90d
Veager <viliussaukstas@gmail.com>
2021-08-16 16:51:44 +0300

    added one operand operators

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
     operator1
     operator as first input
     bracket between number and factorial function
+    remove dependency on innerText
 */
 
 //functions for mathematical operations
@@ -19,6 +20,21 @@ function multiply(op1, op2){
 function divide(op1, op2){
     return op1 / op2;
 }
+function factorial(op1){
+    let result = 1;
+    while(op1>1){
+        result *= op1;
+        op1--;
+    }
+    return result;
+}
+function clear(){
+    displayArray = [""];
+    displayIndex = 0;
+    orderOfOperations = [];
+    prevWasOperator = false;
+    display.innerText = "";
+}
 
 const display = document.querySelector(".cal-display");
 const buttons = document.querySelectorAll(".button");
@@ -32,11 +48,15 @@ let prevWasOperator = false;
 function addInput(e){
     // checks input type and adds to display
     if(e.target.dataset.type === "num"){
+        if(prevWasOperator){
+            displayIndex++;
+            displayArray[displayIndex] = "";
+        }
         display.innerText += e.target.innerText;
         displayArray[displayIndex] += e.target.innerText;
         prevWasOperator = false;
     }
-    else if(e.target.dataset.type === "operator2"){
+    else if(e.target.dataset.type === "operator2"){ // operators which work with 2 operands
         if(prevWasOperator){
             console.log("Two operators in a row")
             return 0;
@@ -59,14 +79,32 @@ function addInput(e){
             priority: operatorPriority,
         }
         orderOfOperations.push(operatorObject);
-
-        displayIndex++;
-        displayArray[displayIndex] = ""; //prepares array element for next input
         prevWasOperator = true;
     }
-    else if(e.target.innerText === "="){
-        calculate();
+    else if(e.target.dataset.type === "operator1"){ // operators which only work with 1 operand
+        if(prevWasOperator){
+            console.log("Two operators in a row")
+            return 0;
+        }
+
+        display.innerText += e.target.innerText;
+        displayIndex++;
+        displayArray[displayIndex] = e.target.innerText;
+
+        let operatorPriority;
+        switch(e.target.innerText){
+            case "!": operatorPriority=3; break;
+        }
+        let operatorObject = {
+            symbol: e.target.innerText,
+            index: displayIndex,
+            priority: operatorPriority,
+        }
+        orderOfOperations.push(operatorObject);
     }
+    else if(e.target.innerText === "clear") clear();
+    else if(e.target.innerText === "=") calculate();
+
     console.log(displayArray);
     console.table(orderOfOperations);
 }
@@ -75,30 +113,40 @@ function calculate(){
     orderOfOperations.sort((a, b) => b.priority-a.priority);
     let result;
     while(orderOfOperations.length>0){
+        let indexAdjust = 0;
         switch(orderOfOperations[0].symbol){
             // does the maths and replaces the operators and operands with the result of the calcualtion
             case "+":{
                 result = add(displayArray[orderOfOperations[0].index-1], displayArray[orderOfOperations[0].index+1]);
                 displayArray.splice(orderOfOperations[0].index-1, 3, result.toString());
+                indexAdjust = 2;
                 break;
             }
             case "-":{ result = subtract(displayArray[orderOfOperations[0].index-1], displayArray[orderOfOperations[0].index+1]);
                 displayArray.splice(orderOfOperations[0].index-1, 3, result.toString());
+                indexAdjust = 2;
                 break;
             }
             case "*":{ result = multiply(displayArray[orderOfOperations[0].index-1], displayArray[orderOfOperations[0].index+1]);
                 displayArray.splice(orderOfOperations[0].index-1, 3, result.toString());
+                indexAdjust = 2;
                 break;
             }    
             case "/":{ result = divide(displayArray[orderOfOperations[0].index-1], displayArray[orderOfOperations[0].index+1]);
                 displayArray.splice(orderOfOperations[0].index-1, 3, result.toString());
+                indexAdjust = 2;
                 break;
-            }       
+            }
+            case "!":{ result = factorial(displayArray[orderOfOperations[0].index-1]);
+                displayArray.splice(orderOfOperations[0].index-1, 2, result.toString());
+                indexAdjust = 1;
+                break;
+            }           
             default: console.log("none of them?");
         }
         //the displayArray was shortened so this updates the indeces in orderOfOperations
         for(let i=1;i<orderOfOperations.length;i++){
-            if(orderOfOperations[0].index<orderOfOperations[i].index) orderOfOperations[i].index-=2; 
+            if(orderOfOperations[0].index<orderOfOperations[i].index) orderOfOperations[i].index-=indexAdjust; 
         }
         orderOfOperations.shift();
     }
